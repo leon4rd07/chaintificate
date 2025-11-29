aedfe1482b238d5c298570522ae2ef2f9f9d3d35
Lee <leandro.belajarkls11@gmail.com>
2025-11-29 07:44:13 +0700

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ["pino", "thread-stream"],
 };
 
 export default nextConfig;
